c697a1ed996055f33df41cc34118441bfb947214
3lton007 <37709148+3lton007@users.noreply.github.com>
2020-02-18 22:13:19 -0500

    Update GitRepo_test.py

--- GitRepo_test.py
+++ GitRepo_test.py
@@ -9,7 +9,7 @@ class TestRepo(unittest.TestCase):
     def test_commit(self):
         repos = [item for item in repo("3lton007")]
 
-        result = ["Repo: GitHubApi567, Commits: 9", "Repo: MyWorld, Commits: 4", "Repo: SSW-567, Commits: 14", "Repo: SSW810, Commits: 11", "Repo: SSW_567, Commits: 4", "Repo: Triangle567, Commits: 8"]
+        result = ["Repo: GitHubApi567, Commits: 11", "Repo: MyWorld, Commits: 4", "Repo: SSW-567, Commits: 14", "Repo: SSW810, Commits: 11", "Repo: SSW_567, Commits: 4", "Repo: Triangle567, Commits: 8"]
 
         self.assertEqual(repos, result)
 
