c06c2868d8d49ebd6db86a48e3e42605bd63f481
3lton007 <37709148+3lton007@users.noreply.github.com>
2020-02-25 23:16:15 -0500

    Update GitRepo_test.py

--- GitRepo_test.py
+++ GitRepo_test.py
@@ -1,15 +1,18 @@
 
 
 import unittest
+from unittest import mock
 
 from GitRepo import repo
 
 class TestRepo(unittest.TestCase):
 
-    def test_commit(self):
+    @mock.patch("urllib.request.urlopen")
+
+    def test_commit(self,other):
         repos = [item for item in repo("3lton007")]
 
-        result = ["Repo: GitHubApi567, Commits: 11", "Repo: MyWorld, Commits: 4", "Repo: SSW-567, Commits: 14", "Repo: SSW810, Commits: 11", "Repo: SSW_567, Commits: 4", "Repo: Triangle567, Commits: 8"]
+        result = ["Repo: CS-546, Commits: 8", "Repo: GitHubApi567, Commits: 11", "Repo: MyWorld, Commits: 4", "Repo: SSW-567, Commits: 14", "Repo: SSW810, Commits: 11", "Repo: SSW_567, Commits: 4", "Repo: Triangle567, Commits: 8"]
 
         self.assertEqual(repos, result)
 
